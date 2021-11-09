e4f1638ea4aaf991bd0c73e019f1e480a0667244
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-11-09 09:52:27 +0530

    Added android ios icons and responsive styles

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,8 +1,13 @@
 import './Hero.css';
+
+//Import necessary images
 import hero from '../../assets/hero.svg';
+import apple from '../../assets/apple.png';
+import android from '../../assets/android.png';
+
 export const Hero = () => {
   return (
-    <section className="container hero">
+    <section className="container flex hero">
       <div className="hero__content spacer">
         <p className="section__title uppercase">Location SDK</p>
         <h1 className="fs-700 hero__title">
@@ -20,9 +25,13 @@ export const Hero = () => {
             Contact Sales
           </a>
         </div>
+        <div className="mobile__sdks flex">
+          <img src={apple} alt="Apple icon" width="25" height="25" />
+          <img src={android} alt="Android icon" width="30" height="30" />
+        </div>
       </div>
       <div className="hero__image">
-        <img src={hero} alt="" />
+        <img src={hero} alt="Woman boarding a blue car" />
       </div>
     </section>
   );
